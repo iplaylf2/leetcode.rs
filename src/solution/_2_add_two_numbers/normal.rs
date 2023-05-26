39a8d565740b467770634135e448910aa8cd7f5a
i-play-lf2 <914637118@qq.com>
2023-05-26 09:43:41 +0000

    improve solution 2

--- src/solution/_2_add_two_numbers/normal.rs
+++ src/solution/_2_add_two_numbers/normal.rs
@@ -7,80 +7,86 @@ use std::{mem, ops::DerefMut};
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
-        l2: Option<Box<ListNode>>,
+        mut l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
+        let mut head = l1;
+
+        let (current, carry) = Self::braid(head.as_mut().unwrap(), l2.as_mut().unwrap());
+
+        Self::smooth_out(current, carry);
+
+        return head;
+    }
+
+    fn braid<'a>(
+        mut l1: &'a mut Box<ListNode>,
+        mut l2: &'a mut Box<ListNode>,
+    ) -> (&'a mut Option<Box<ListNode>>, i32) {
         let mut carry = 0;
 
-        let mut prefix = ListNode::new(0);
-        let mut final_node = &mut prefix;
-
-        let mut l1 = l1;
-        let mut l2 = l2;
-
         loop {
-            let mut l1_box = l1.unwrap();
-
             let ListNode {
                 val: l1_val,
                 next: l1_next,
-            } = l1_box.deref_mut();
+            } = l1.deref_mut();
 
             let ListNode {
                 val: l2_val,
                 next: l2_next,
-            } = l2.as_mut().unwrap().deref_mut();
+            } = l2.deref_mut();
 
-            let x = *l1_val + *l2_val + carry;
+            let (x, c) = match *l1_val + *l2_val + carry {
+                x if x < 10 => (x, 0),
+                x => (x - 10, 1),
+            };
+            *l1_val = x;
+            carry = c;
 
-            *l1_val = if x < 10 {
-                carry = 0;
-                x
-            } else {
-                carry = 1;
-                x - 10
+            l1 = match l1_next.as_ref() {
+                Some(next) => unsafe {
+                    let next = next as *const Box<ListNode> as *mut Box<ListNode>;
+                    &mut *next
+                },
+                None => {
+                    *l1_next = mem::take(l2_next);
+                    return (l1_next, carry);
+                }
             };
 
-            l1 = mem::take(l1_next);
-            l2 = mem::take(l2_next);
+            l2 = match l2_next {
+                Some(next) => next,
+                None => {
+                    return (l1_next, carry);
+                }
+            };
+        }
+    }
 
-            final_node.next = Some(l1_box);
-            final_node = final_node.next.as_mut().unwrap();
-
-            if l1.is_none() {
-                l1 = l2;
-                break;
-            }
-
-            if l2.is_none() {
-                break;
-            }
+    fn smooth_out(mut current: &mut Option<Box<ListNode>>, carry: i32) {
+        if 0 == carry {
+            return;
         }
 
-        while let Some(mut l1_box) = l1 {
-            let ListNode { val, next: l1_next } = l1_box.as_mut();
-
-            let x = *val + carry;
-
-            if x < 10 {
-                *val = x;
-                
-                final_node.next = Some(l1_box);
-                return prefix.next;
-            } else {
-                carry = 1;
-
-                *val = 0;
-                l1 = mem::take(l1_next);
-
-                final_node.next = Some(l1_box);
-                final_node = final_node.next.as_mut().unwrap();
+        loop {
+            current = match current {
+                Some(node) => {
+                    let ListNode { val, next } = node.deref_mut();
+                    match *val + 1 {
+                        x if x < 10 => {
+                            *val = x;
+                            break;
+                        }
+                        _ => {
+                            *val = 0;
+                            next
+                        }
+                    }
+                }
+                None => {
+                    *current = Some(Box::new(ListNode::new(1)));
+                    break;
+                }
             }
         }
-
-        if 1 == carry {
-            final_node.next = Some(Box::new(ListNode::new(1)));
-        }
-
-        prefix.next
     }
 }
